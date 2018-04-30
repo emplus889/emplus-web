702d30a97f136a2cb47200311d23f62d3d8b65d7
t0n1zz <tony762843@yahoo.co.id>
2018-04-30 20:17:54 +0700

    fixing laravel passport control panel component layout

--- resources/views/passport.blade.php
+++ resources/views/passport.blade.php
@@ -2,10 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <passport-clients></passport-clients>
-        <passport-authorized-clients></passport-authorized-clients>
-        <passport-personal-access-tokens></passport-personal-access-tokens>
-    </div>
+    <passport-clients></passport-clients>
+    <passport-authorized-clients></passport-authorized-clients>
+    <passport-personal-access-tokens></passport-personal-access-tokens>
 </div>
 @endsection
